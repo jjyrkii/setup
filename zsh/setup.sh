325595b4aca6dcf683a9137600cf64663bc0d29f
jjyrkii <jyrki1887@gmail.com>
2025-05-15 14:03:54 +0200

    init

---
+++ zsh/setup.sh
@@ -0,0 +1,23 @@
+echo "Installing and configuring ZSH..."
+
+sudo pacman -S --noconfirm zsh
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  rm -rf "$HOME/.oh-my-zsh"
+fi
+
+INSTALL_SCRIPT="$(mktemp)"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o "$INSTALL_SCRIPT"
+
+# Remove the 'exec zsh' line so our script can continue
+sed -i '/exec zsh -l/d' "$INSTALL_SCRIPT"
+
+# Now run the modified installer
+bash "$INSTALL_SCRIPT"
+rm "$INSTALL_SCRIPT"
+
+rm -f "$HOME/.zshrc"
+ln -s "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
