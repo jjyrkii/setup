30b6df1a973b4a0639db357b3a0bf4eee7170407
jjyrkii <jyrki1887@gmail.com>
2025-05-15 14:04:57 +0200

    done message

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -21,3 +21,5 @@ ln -s "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Done installing ZSH."
